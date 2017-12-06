bcb8329a8caea2c6f14f8d0c5a05a3f716e5f4b1
jakubKuznik <kuznja16@gmail.com>
2017-12-06 21:19:28 +0100

    Add files via upload
    z omluvou a dlouhým bádaním po internetu jsem nezjistil jak jse přidávají čáry a jejich parametry

---
+++ GhetoSAS_mysql_create.sql
@@ -0,0 +1,36 @@
+CREATE TABLE `Uživatel` (
+
+);
+
+CREATE TABLE `Přítomnost '(
+
+);
+
+CREATE TABLE `Lekce '(
+
+);
+
+CREATE TABLE `grade` (
+
+);
+
+CREATE TABLE `Studenti '(
+
+);
+
+CREATE TABLE `subject '(
+
+);
+
+CREATE TABLE `Sezóna` (
+
+);
+
+CREATE TABLE `clazz` (
+
+);
+
+CREATE TABLE `groups` (
+
+);
+
